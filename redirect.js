a64a3c0fdb32919ac254bcf0ec5b46e6b42f0853
tejas <tejasdeshmukh320@gmail.com>
2024-09-26 12:36:25 +0530

    new html pages

---
+++ redirect.js
@@ -0,0 +1,23 @@
+let gym1 = document.querySelector('#gym1redirect');
+let gym2 = document.querySelector('#gym2redirect');
+let gym3 = document.querySelector('#gym3redirect');
+let gym4 = document.querySelector('#gym4redirect');
+
+gym1.addEventListener("click", function() {
+    window.location.href = "/gym1/gym1.html"; 
+});
+
+
+
+gym2.addEventListener("click", function() {
+    window.location.href = "/gym1/gym2.html"; 
+});
+
+
+gym3.addEventListener("click", function() {
+    window.location.href = "/gym1/gym3.html"; 
+});
+
+gym4.addEventListener("click", function() {
+    window.location.href = "/gym1/gym4.html"; 
+});
